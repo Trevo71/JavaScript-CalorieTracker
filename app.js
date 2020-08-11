4d83c917e329a27f9c5e5a15e131d6387c222eb3
Trevo71 <67796759+Trevo71@users.noreply.github.com>
2020-08-11 13:40:06 -0500

    populate html with the items within array

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 
 // Item Controller
 const ItemCtrl = (function(){
+
     //Item Constructor 
     const Item = function(id,name,calories){
         this.id = id;
         this.name = name;
         this.calories = calories;
     }
-    
    // Data Structure / State
    const data = {
        items: [
            {id: 0, name: 'Steak Dinner', calories: 1200},
-           {id: 0, name: 'Cook', calories: 1200},
+           {id: 0, name: 'Cookie', calories: 1200},
            {id: 0, name: 'Steak Dinner', calories: 1200}
        ],
        currentItem: null,
@@ -21,7 +21,11 @@ const ItemCtrl = (function(){
    }
 
    return {
-       logData: function(){
+
+    getItems: function(){
+           return data.items;
+    },
+    logData: function(){
            return data;
        }
    }
@@ -31,10 +35,27 @@ const ItemCtrl = (function(){
 // UI Controller
 const UICtrl = (function(){
 
-    return {
-
+    const UISelectors = {
+        itemList: '#item-list'
     }
 
+    return {
+        populateItemList: function(items){
+            let html = '';
+
+            items.forEach(function(item){
+            html += `
+            <li class="collection-item" id="${item.id}">
+            <strong>${item.name}: </strong><em>${item.calories} Calories</em>
+            <a href="" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+          </li>
+            `
+            });
+
+            //Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+    }
 })();
 
 
@@ -45,8 +66,15 @@ const App = (function(ItemCtrl,UICtrl){
     return {
         init: function(){
             console.log('Initializing App...');
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+            console.log(items);
+
+            UICtrl.populateItemList(items)
         }
     }
 
 })(ItemCtrl, UICtrl);
+
+App.init();
 
